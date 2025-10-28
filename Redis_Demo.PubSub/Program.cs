3cd1903b427444b8f55dbb4e89babe8035cdc4f3
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-10-29 00:46:37 +0300

    Redis Pub/Sub

---
+++ Redis_Demo.PubSub/Program.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.Write("Enter a message to publish (or 'exit' to quit):");
+    string? message = Console.ReadLine();
+    await subscriber.PublishAsync("demoChannel", message);
+}
